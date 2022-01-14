07f4079334b4b7d3d6770fc06d42431f78f7885d
youssef <youssefmohamedsabry36@gmail.com>
2022-01-14 05:57:17 +0200

    adding consolelog for testing

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -5,9 +5,13 @@ export async function middleware(req) {
   const token = await getToken({ req, secret: process.env.JWT_SECRET });
   const { pathname } = req.nextUrl;
   if (pathname.includes("/api/auth") || token) {
+    console.log("JWWWWWWWWT" + token);
+    console.log(pathname);
     return NextResponse.next();
   }
   if (!token && pathname !== "/login") {
+    console.log(pathname);
+    console.log("JWWWWWWWWT" + token);
     return NextResponse.redirect("/login");
   }
 }
